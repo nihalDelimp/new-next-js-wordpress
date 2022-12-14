1d7f3be9dfe30e4e4b50535653d51a251e8376bc
nihalDelimp <nihal@delimp.com>
2022-12-14 19:54:27 +0530

    Data center length eror

--- components/Services/DataCenter.js
+++ components/Services/DataCenter.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Footer from '../Footer/WrapperFooter'
 
 function DataCenter({ section1, section2, section3, section4, section5 }) {
-  
+
    return (
       <div id="fullpage" style={{ backgroundColor: 'rgb(2,0,36)' }}  >
-         <section id="data-center-section_1" className="section bg-gradient bg-size-cover bg-no-repeat bg-center fp-section fp-table active fp-completely" style={{backgroundImage: `url(${section1?.backgroundImage?.sourceUrl}) !important`}}>
+         <section id="data-center-section_1" className="section bg-gradient bg-size-cover bg-no-repeat bg-center fp-section fp-table active fp-completely" style={{ backgroundImage: `url(${section1?.backgroundImage?.sourceUrl}) !important` }}>
             <div id="data-center-container" className="container-lg pt-4 pb-4">
                <div className="mx-lg-6 mx-4 pt-lg-0 pb-lg-0 pt-5 pb-5">
                   <section id="breadcrum-section">
@@ -24,13 +24,13 @@ function DataCenter({ section1, section2, section3, section4, section5 }) {
 
                      <div className="col-md-12 align-self-center">
                         <h1 className="text-white font-bold bottom-buffer-1point5rem mt-2">{section1?.heading}</h1>
-                        {section1.descriptionSection && section1.descriptionSection.length > 0 &&
-                           section1.descriptionSection.map((item, index) =>
+                        {section1?.descriptionSection && section1?.descriptionSection.length > 0 &&
+                           section1?.descriptionSection.map((item, index) =>
                               <p key={index + 1} className="text-white big mb-2 mt-2">{item.description}</p>
                            )}
                         <ul className="text-left mt-3 mb-3">
-                           {section1.listSection && section1.listSection.length > 0 &&
-                              section1.listSection.map((item, index) =>
+                           {section1?.listSection && section1?.listSection.length > 0 &&
+                              section1?.listSection.map((item, index) =>
                                  <li key={index + 1} className="text-white big">{item.listItems}</li>
                               )}
                         </ul>
@@ -50,23 +50,23 @@ function DataCenter({ section1, section2, section3, section4, section5 }) {
                      </div>
                   </div>
                   <div className="row pt-2 pb-2">
-                     {section2.descriptionWithIcon && section2.descriptionWithIcon.length > 0 &&
-                     section2.descriptionWithIcon.map((item , i) =>
-                     <>
-                     <div className="col-lg-1 mt-2 mb-2 text-center text-lg-left">
-                        <div className="d-flex justify-content-center">
-                           <span className="px-2 py-1 bg-primary text-white font-small font-bold extra-large rounded-circle">{item.icon}</span>
-                        </div>
-                     </div>
-                     <div className="col-lg-5 mt-2 mb-2 text-center text-lg-left">
-                        <div className="row">
-                           <div className="col-sm-12">
-                              <h2 className="text-white font-bold mb-2 mt-2">{item.heading}</h2>
-                              <p className="text-white big mb-2 mt-2">{item.description}</p>
-                           </div>
-                        </div>
-                     </div>
-                     </>)}
+                     {section2?.descriptionWithIcon && section2?.descriptionWithIcon.length > 0 &&
+                        section2?.descriptionWithIcon.map((item, i) =>
+                           <>
+                              <div className="col-lg-1 mt-2 mb-2 text-center text-lg-left">
+                                 <div className="d-flex justify-content-center">
+                                    <span className="px-2 py-1 bg-primary text-white font-small font-bold extra-large rounded-circle">{item.icon}</span>
+                                 </div>
+                              </div>
+                              <div className="col-lg-5 mt-2 mb-2 text-center text-lg-left">
+                                 <div className="row">
+                                    <div className="col-sm-12">
+                                       <h2 className="text-white font-bold mb-2 mt-2">{item.heading}</h2>
+                                       <p className="text-white big mb-2 mt-2">{item.description}</p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </>)}
                   </div>
                </div>
             </div>
@@ -78,10 +78,10 @@ function DataCenter({ section1, section2, section3, section4, section5 }) {
                   <div className="row pt-2 pb-2">
                      <div className="col-lg-12 text-center text-lg-left mt-2 mb-2">
                         <h1 className="text-white font-bold bottom-buffer-2point5rem">{section3?.heading}</h1>
-                        {section3 && section3.descriptionSection.length > 0 &&
-                         section3.descriptionSection.map((item , index) => 
-                        <p key = {index} className="text-white big mb-2 mt-2">{item.description}</p>
-                        )} 
+                        {section3 && section3?.descriptionSection.length > 0 &&
+                           section3.descriptionSection.map((item, index) =>
+                              <p key={index} className="text-white big mb-2 mt-2">{item.description}</p>
+                           )}
                      </div>
                   </div>
                </div>
@@ -94,13 +94,13 @@ function DataCenter({ section1, section2, section3, section4, section5 }) {
                   <div className="row pt-2 pb-2">
                      <div className="col-lg-12 text-center text-lg-left mt-2 mb-2">
                         <h1 className="text-white font-bold bottom-buffer-2point5rem">{section4?.heading}</h1>
-                        {section4 && section4.descriptionWithSubHeading.length > 0 &&
-                         section4.descriptionWithSubHeading.map((item , index) =>
-                        <> 
-                        <h3 className="text-white font-bold mb-2">{item.heading}</h3>
-                        <p className="text-white big mb-2 mt-2">{item.description}</p>
-                        </>
-                        )} 
+                        {section4 && section4?.descriptionWithSubHeading.length > 0 &&
+                           section4?.descriptionWithSubHeading.map((item, index) =>
+                              <>
+                                 <h3 className="text-white font-bold mb-2">{item.heading}</h3>
+                                 <p className="text-white big mb-2 mt-2">{item.description}</p>
+                              </>
+                           )}
                      </div>
                   </div>
                </div>
@@ -112,14 +112,14 @@ function DataCenter({ section1, section2, section3, section4, section5 }) {
                <div className="mx-lg-6 mx-4 pt-lg-0 pb-lg-0 pt-5 pb-5">
                   <div className="row pt-2 pb-2">
                      <div className="col-lg-12 text-center text-lg-left mt-2 mb-2">
-                     <h1 className="text-white font-bold bottom-buffer-2point5rem">{section5?.heading}</h1>
-                        {section5 && section5.descriptionWithSubHeading.length > 0 &&
-                         section5.descriptionWithSubHeading.map((item , index) =>
-                        <> 
-                        <h3 className="text-white font-bold mb-2">{item.heading}</h3>
-                        <p className="text-white big mb-2 mt-2">{item.description}</p>
-                        </>
-                        )} 
+                        <h1 className="text-white font-bold bottom-buffer-2point5rem">{section5?.heading}</h1>
+                        {section5 && section5?.descriptionWithSubHeading.length > 0 &&
+                           section5?.descriptionWithSubHeading.map((item, index) =>
+                              <>
+                                 <h3 className="text-white font-bold mb-2">{item.heading}</h3>
+                                 <p className="text-white big mb-2 mt-2">{item.description}</p>
+                              </>
+                           )}
                      </div>
                   </div>
                </div>
@@ -149,7 +149,7 @@ function DataCenter({ section1, section2, section3, section4, section5 }) {
                            <div className="align-bottom-content-lg "><a className="btn btn-primary" href="https://jobs.alfanar.com/alfanar/go/All-Openings/4442101/" target="_blank" alt="Discover Opportunities">Discover Opportunities</a></div>
                         </div>
                         <div className="col-lg-5 align-self-center text-lg-right text-center">
-                           <img className="w-100" src="https://alfanar-digital-solution.azurewebsites.net/public/final-images/careers-header-img.png"/>
+                           <img className="w-100" src="https://alfanar-digital-solution.azurewebsites.net/public/final-images/careers-header-img.png" />
                         </div>
                      </div>
                   </div>
